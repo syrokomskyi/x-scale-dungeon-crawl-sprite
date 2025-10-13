002f83c92664b784f04db938d4b9a64160404499
Andrii Syrokomskyi <signmotion@gmail.com>
2025-10-13 02:36:24 +0200

    Introduces format and compress.

--- bin/upacayl/upscale_sprites.cmd
+++ bin/upacayl/upscale_sprites.cmd
@@ -4,8 +4,8 @@ setlocal enabledelayedexpansion
 echo Starting sprite upscaling process...
 echo.
 
-::set "MODEL_NAME=digital-art-4x"
-set "MODEL_NAME=upscayl-standard-4x"
+set "MODEL_NAME=digital-art-4x"
+::set "MODEL_NAME=upscayl-standard-4x"
 
 set "SCALE=4"
 set "SCALE_FOLDER=128x"
@@ -14,6 +14,9 @@ set "INPUT_DIR=C:\projects\x-scale-dungeon-crawl-sprite\sprites\test\original"
 set "OUTPUT_BASE=C:\projects\x-scale-dungeon-crawl-sprite\sprites\test\%MODEL_NAME%\%SCALE_FOLDER%"
 set "UPSCALE_BIN=C:\Program Files\Upscayl\resources\bin\upscayl-bin.exe"
 set "MODELS_DIR=C:\Program Files\Upscayl\resources\models"
+
+set "FORMAT=webp"
+set "COMPRESS=0"
 
 :: Create output base directory if it doesn't exist
 if not exist "%OUTPUT_BASE%" mkdir "%OUTPUT_BASE%"
@@ -32,8 +35,8 @@ for /d %%d in ("%INPUT_DIR%\*") do (
     
     if not exist "!OUTPUT_SUBDIR!" mkdir "!OUTPUT_SUBDIR!"
     
-    "%UPSCALE_BIN%" -i "!INPUT_SUBDIR_UNIX!" -o "!OUTPUT_SUBDIR_UNIX!" -m "%MODELS_DIR%" -n "%MODEL_NAME%" -s %SCALE%
-    
+    "%UPSCALE_BIN%" -i "!INPUT_SUBDIR_UNIX!" -o "!OUTPUT_SUBDIR_UNIX!" -m "%MODELS_DIR%" -n "%MODEL_NAME%" -s %SCALE% -f %FORMAT% -c %COMPRESS%
+
     if !errorlevel! equ 0 (
         echo   SUCCESS: Folder !SUBDIR! processed
     ) else (
