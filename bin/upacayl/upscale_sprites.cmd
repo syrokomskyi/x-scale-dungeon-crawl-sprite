4476c1c4a6e2b0716e00119c5e652f3bc970c5e7
Andrii Syrokomskyi <signmotion@gmail.com>
2025-10-13 03:04:21 +0200

    Enhanced the script for work with any depth folders.

--- bin/upacayl/upscale_sprites.cmd
+++ bin/upacayl/upscale_sprites.cmd
@@ -27,28 +27,48 @@ set "COMPRESS=0"
 :: Create output base directory if it doesn't exist
 if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
 
-:: Process each subdirectory
-for /d %%d in ("%INPUT_DIR%\*") do (
-    set "SUBDIR=%%~nxd"
-    set "INPUT_SUBDIR=%%d"
-    set "OUTPUT_SUBDIR=%OUTPUT_DIR%\!SUBDIR!"
-    set "INPUT_SUBDIR_UNIX=!INPUT_SUBDIR:\=/!"
-    set "OUTPUT_SUBDIR_UNIX=!OUTPUT_SUBDIR:\=/!"
-    
-    echo Processing folder: "!SUBDIR!"
-    echo   Input:  !INPUT_SUBDIR_UNIX!
-    echo   Output: !OUTPUT_SUBDIR_UNIX!
-    
-    if not exist "!OUTPUT_SUBDIR!" mkdir "!OUTPUT_SUBDIR!"
-    
-    "%UPSCALE_BIN%" -i "!INPUT_SUBDIR_UNIX!" -o "!OUTPUT_SUBDIR_UNIX!" -m "%MODELS_DIR%" -n "%MODEL_NAME%" -s %SCALE% -f %FORMAT% -c %COMPRESS%
+:: Get absolute paths for INPUT_DIR and OUTPUT_DIR
+pushd "%INPUT_DIR%"
+set "INPUT_DIR_ABS=%CD%"
+popd
 
-    if !errorlevel! equ 0 (
-        echo   SUCCESS: Folder !SUBDIR! processed
-    ) else (
-        echo   ERROR: Failed to process folder !SUBDIR!
+pushd "%OUTPUT_DIR%"
+set "OUTPUT_DIR_ABS=%CD%"
+popd
+
+:: Process all subdirectories recursively
+for /r "%INPUT_DIR%" %%d in (.) do (
+    set "CURRENT_DIR=%%d"
+    set "CURRENT_DIR=!CURRENT_DIR:~0,-2!"
+    
+    :: Calculate relative path from INPUT_DIR
+    set "REL_PATH=!CURRENT_DIR:%INPUT_DIR_ABS%=!"
+    
+    :: Skip if it's the root directory itself
+    if not "!REL_PATH!"=="" (
+        :: Remove leading backslash
+        if "!REL_PATH:~0,1!"=="\" set "REL_PATH=!REL_PATH:~1!"
+        
+        set "INPUT_FOLDER=!CURRENT_DIR!"
+        set "OUTPUT_FOLDER=%OUTPUT_DIR_ABS%\!REL_PATH!"
+        set "INPUT_FOLDER_UNIX=!INPUT_FOLDER:\=/!"
+        set "OUTPUT_FOLDER_UNIX=!OUTPUT_FOLDER:\=/!"
+        
+        echo Processing folder: "!REL_PATH!"
+        echo   Input:  !INPUT_FOLDER_UNIX!
+        echo   Output: !OUTPUT_FOLDER_UNIX!
+        
+        if not exist "!OUTPUT_FOLDER!" mkdir "!OUTPUT_FOLDER!"
+        
+        "%UPSCALE_BIN%" -i "!INPUT_FOLDER_UNIX!" -o "!OUTPUT_FOLDER_UNIX!" -m "%MODELS_DIR%" -n "%MODEL_NAME%" -s %SCALE% -f %FORMAT% -c %COMPRESS%
+        
+        if !errorlevel! equ 0 (
+            echo   SUCCESS: Folder !REL_PATH! processed
+        ) else (
+            echo   ERROR: Failed to process folder !REL_PATH!
+        )
+        echo.
     )
-    echo.
 )
 
 echo.
