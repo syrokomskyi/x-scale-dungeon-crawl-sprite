3698fce10718ee480e49f730d3a1e28541d9ac62
Andrii Syrokomskyi <signmotion@gmail.com>
2025-10-28 19:37:59 +0100

    Fixed a lost styles on Github Pages.

--- apps/site-1/astro.config.mjs
+++ apps/site-1/astro.config.mjs
@@ -2,7 +2,7 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://syrokomskyi.github.io/x-scale-dungeon-crawl-sprite/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/x-scale-dungeon-crawl-sprite/" : "",
   integrations: [tailwind()],
-});
+}));
