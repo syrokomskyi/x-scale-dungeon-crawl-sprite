c162c025f70a81161794aa470fcdff6bdcacd5fd
Andrii Syrokomskyi <signmotion@gmail.com>
2025-10-29 19:15:52 +0100

    Fixed after upgrade TailwindCss and Ko.

--- apps/site-1/astro.config.mjs
+++ apps/site-1/astro.config.mjs
@@ -1,9 +1,11 @@
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://syrokomskyi.github.io",
   base: import.meta.env.PROD ? "/x-scale-dungeon-crawl-sprite" : "/",
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
